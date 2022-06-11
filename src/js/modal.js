fb21dedeabc61dedc2106a34a8e5cdd153d0b9b0
Alex <100074196+Pupsikk@users.noreply.github.com>
2022-06-11 17:18:42 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,14 @@
 (() => {
 const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    openMobileModalBtn: document.querySelector('[data-mobile-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('[data-body-overflow]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
-refs.openMobileModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scrol');
+    refs.modal.classList.toggle("is-hidden");
 }
 })();
