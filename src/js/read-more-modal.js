dd5b599d2b59ab5e0deb3aa4e9e567b3aa5289d6
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-06-13 22:34:09 +0300

    add some styles and media queries

--- src/js/read-more-modal.js
+++ src/js/read-more-modal.js
@@ -1,8 +1,8 @@
 (() => {
 const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-read-more-modal-open]"),
+    closeModalBtn: document.querySelector("[data-read-more-modal-close]"),
+    modal: document.querySelector("[data-read-more-modal]"),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
