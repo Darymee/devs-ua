b1daf99409020a26cbc50e9a5d1df6aa8a406c73
Darymee <98210490+Darymee@users.noreply.github.com>
2022-06-13 19:36:18 +0000

    Deploying to gh-pages from @ Darymee/devs-ua@95b225c5b3d53b080f1bdad2a77c604ff66f5f23 🚀

---
+++ index.b5875329.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-read-more-modal-open]"),closeModalBtn:document.querySelector("[data-read-more-modal-close]"),modal:document.querySelector("[data-read-more-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.b5875329.js.map
