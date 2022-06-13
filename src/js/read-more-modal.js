14adf6cbcc97c6496e6bc654c565da853838533a
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-06-13 23:43:43 +0300

    add no-scroll in js

--- src/js/read-more-modal.js
+++ src/js/read-more-modal.js
@@ -10,5 +10,6 @@ refs.closeModalBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
 }
 })();
