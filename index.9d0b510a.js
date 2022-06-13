62152d8646b0c3575f117b4d2d458eb9d6232e47
Darymee <98210490+Darymee@users.noreply.github.com>
2022-06-13 14:27:34 +0000

    Deploying to gh-pages from @ Darymee/devs-ua@591dcf72fff96a7643e531353b53e302d53ac523 🚀

---
+++ index.9d0b510a.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector(".data-modal-open"),closeModalBtn:document.querySelector(".data-modal-close"),modal:document.querySelector(".data-modal"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.9d0b510a.js.map
